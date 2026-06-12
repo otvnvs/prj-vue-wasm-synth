31fa05a6ee28b98f74a52a0b8e3c831d0f4fa1b9
skullquake <ockert8080@gmail.com>
2026-06-12 17:32:41 +0200

    initial commit
    Signed-off-by: skullquake <ockert8080@gmail.com>

--- bak/001/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-emcc generator.c -o waveform.js \
-  -s EXPORTED_FUNCTIONS='["_generate_sine_wave", "_generate_square_wave", "_stream_sine_wave", "_reset_stream_phase", "_malloc", "_free"]' \
-  -s EXPORTED_RUNTIME_METHODS='["HEAPF32"]' \
-  -s STANDALONE_WASM=1 \
-  --no-entry \
-  -O3
